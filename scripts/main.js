b3a8ee8c6ed97d182d65290ad8d5d5d2fd433900
hTKzmak <ineedrtx@gmail.com>
2024-02-24 17:15:38 +0300

    adding menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,15 @@
-let buttonMenu = document.querySelector('.header-mobile-menu');
+const buttonMenu = document.querySelector('.header-mobile-btn');
+const headerMenuMobile = document.querySelector('.header-menu-mobile');
+const navBtnImg = document.querySelector('#nav-btn-img');
 
-buttonMenu.addEventListener('click', function(){
-    alert('Open menu');
-})
+buttonMenu.addEventListener('click', function () {
+    if(headerMenuMobile.classList.toggle('open')){
+        navBtnImg.src = './images/close_menu.svg'
+    } 
+    else{
+        navBtnImg.src = './images/menu.svg'
+    }
+});
 
 AOS.init({
     disable: 'phone',
